6355844ce190ae2a6fceb4d0bf62cf25f487e752
Michael Häfliger <michael.haefliger@mesch.ch>
2019-09-14 01:09:10 +0200

    slap in some example code

---
+++ blocks/result_list/view.php
@@ -0,0 +1,16 @@
+<?php
+defined('C5_EXECUTE') or die('Access Denied.');
+
+/*
+ * This file is part of Block Boilerplate.
+ *
+ * (c) Oliver Green <oliver@c5labs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+?>
+<div class="block-boilerplate">
+    <span>Block Boilerplate</span>
+</div>
--- blocks/seat_map_2/add.php
+++
@@ -1,1 +0,0 @@
-<?php $this->inc('form.php');
