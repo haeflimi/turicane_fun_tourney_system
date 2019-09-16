3c903db05708540b3d807a20491eba5224843247
Michael Häfliger <michael.haefliger@mesch.ch>
2019-09-16 22:55:09 +0200

    change some views

--- blocks/tfts_pool_overview/view.php
+++ blocks/tfts_pool_overview/view.php
@@ -1,2 +1,7 @@
 <?php defined('C5_EXECUTE') or die('Access Denied.');
 $fh = Core::make('helper/form')?>
+
+
+<div class="row">
+    <h2>Pool Overview</h2>
+</div>
--- blocks/tfts_team_manager/view.php
+++ blocks/tfts_team_manager/view.php
@@ -3,28 +3,5 @@
 ?>
 
 <div class="row">
-    <div class="col-md-4 col-sm-8 col-xs-6">
-        <div class="box fact animated fadeIn delay-1" data-perc="<?=$membersNum?>">
-            <div class="big-text factor""></div>
-            <div class="description"><i class="fa fa-user"></i>
-                Vereinsmitglieder
-            </div>
-        </div>
-    </div>
-    <div class="col-md-4 col-sm-8 col-xs-6">
-        <div class="box fact animated fadeIn delay-1" data-perc="<?=$balance?>">
-            <div class="big-text"><span class="factor"></span>.- SFr.</div>
-            <div class="description">Vereinsvermögen</div>
-        </div>
-    </div>
-    <div class="col-md-4 col-sm-8 col-xs-6">
-        <div class="box fact animated fadeIn delay-1" data-perc="<?=$averageAge?>">
-            <div class="description">
-                Altersdurchschnitt
-            </div>
-            <div class="big-text">
-                <span class="factor"></span> J.
-            </div>
-        </div>
-    </div>
+    <h2>Pool Overview</h2>
 </div>
