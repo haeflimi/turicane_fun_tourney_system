7f648b332d457c7f00fbb516ecea62f3f3c95c5a
Freezer <Teazl@Freezer.home>
2019-10-06 15:41:20 +0200

    Added method skeletons.

--- src/Tfts/Tfts.php
+++ src/Tfts/Tfts.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Tfts;
 
 use Concrete\Core\User\User;
@@ -13,14 +14,13 @@ use Tfts\Entity\Lan;
  * Class Tfts
  * @package TuricaneFunTourneySystem
  */
-class Tfts
-{
+class Tfts {
+
     protected $db; // Database Connection we want to use in this Class
     protected $qb; // Doctrine Query Builder
     protected $em; // Doctrine Entity Manager
 
-    public function __construct($obj = null)
-    {
+    public function __construct($obj = null) {
         // Get the Connection to the right MySQL Database. configured in /application/config/database.php
         $this->db = Database::connection('turicane_fun_tourney_system');
         // Create a Query Builder Instance using our new Connection
@@ -40,9 +40,9 @@ class Tfts
      * Basic Entities and Relations for the TFTS Tables are alreday in place here:
      * src/Entity
      */
-    public function getUserRanking(User $user, Lan $lan){
+    public function getUserRanking(User $user, Lan $lan) {
         $repository = $this->em->getRepository('Tfts\Entity\Ranking');
-        $ranking = $repository->findOneBy(['lan'=>$lan->getId(),'user'=>$user->getUserID()]);
+        $ranking = $repository->findOneBy(['lan' => $lan->getId(), 'user' => $user->getUserID()]);
         return $ranking;
     }
 
@@ -53,14 +53,14 @@ class Tfts
      * @param string $event
      * @return mixed
      */
-    public function getUserRankingSQL(User $user, $event = 'Turicane 17'){
+    public function getUserRankingSQL(User $user, $event = 'Turicane 17') {
 
         // Get all recorded TFTS Rankings for the User Freezer
         $query = $this->db->executeQuery('SELECT t_user.user_name, t_lan_tfts_ranking.points, t_lan.lan_name
             FROM t_lan_tfts_ranking
             INNER JOIN t_lan ON t_lan_tfts_ranking.lan_id = t_lan.lan_id
             INNER JOIN t_user ON t_lan_tfts_ranking.user_id = t_user.user_id
-            WHERE t_user.user_name = ? AND t_lan.lan_name = ?', [$user->getUserName(),$event]);
+            WHERE t_user.user_name = ? AND t_lan.lan_name = ?', [$user->getUserName(), $event]);
         $result = $query->fetchAll();
         return $result;
     }
@@ -72,64 +72,155 @@ class Tfts
      * @param string $event
      * @return mixed
      */
-    public function getUserRankingQueryBuilder(User $user, $event = 'Turicane 17')
-    {
+    public function getUserRankingQueryBuilder(User $user, $event = 'Turicane 17') {
         // Doing the Same thing using Doctrines Query builder takes care of things like SQL injection is one plus of
         // using that method
         // https://www.doctrine-project.org/projects/doctrine-dbal/en/2.9/reference/query-builder.html
         $query = $this->qb
-            ->select('u.user_name', 'l.lan_name', 'ltsr.points')
-            ->from('t_lan_tfts_ranking', 'ltsr')
-            ->join('ltsr','t_user', 'u', 'u.user_id = ltsr.user_id')
-            ->join('ltsr', 't_lan', 'l', 'l.lan_id = ltsr.lan_id')
-            ->where('u.user_name = ?')
-            ->andWhere('l.lan_name = ?')
-            ->setParameter(0, $user->getUserName())
-            ->setParameter(1, $event);
+                ->select('u.user_name', 'l.lan_name', 'ltsr.points')
+                ->from('t_lan_tfts_ranking', 'ltsr')
+                ->join('ltsr', 't_user', 'u', 'u.user_id = ltsr.user_id')
+                ->join('ltsr', 't_lan', 'l', 'l.lan_id = ltsr.lan_id')
+                ->where('u.user_name = ?')
+                ->andWhere('l.lan_name = ?')
+                ->setParameter(0, $user->getUserName())
+                ->setParameter(1, $event);
 
         $result = $query->execute()->FetchAll();
         return $result;
     }
 
-    public function processTrackmaniaData()
-    {
-        $data = [$_REQUEST,'Data processed!'];
+    /**
+     * The given user wants to join the pool of the given game.
+     * 
+     * @param \Tfts\Game $game
+     * @param User $user
+     */
+    public function joinUserPool(Game $game, User $user) {
+        
+    }
+
+    /**
+     * The given user wants to leave the pool of the given game.
+     * 
+     * @param \Tfts\Game $game
+     * @param User $user
+     */
+    public function leaveUserPool(Game $game, User $user) {
+        
+    }
+
+    /**
+     * A user challenges another user for a duel of the given game.
+     * 
+     * @param \Tfts\Game $game
+     * @param User $challenger
+     * @param User $challenged
+     */
+    public function challengeUser(Game $game, User $challenger, User $challenged) {
+        
+    }
+
+    /**
+     * The challenger withdraws the challenge.
+     * 
+     * @param \Tfts\Match $match
+     * @param User $challenger
+     */
+    public function withdrawUserChallenge(Match $match, User $challenger) {
+        
+    }
+
+    /**
+     * The challenged accepts the challenge.
+     * 
+     * @param \Tfts\Match $match
+     * @param User $challenged
+     */
+    public function acceptUserChallenge(Match $match, User $challenged) {
+        
+    }
+
+    /**
+     * The challenged declines the challenge.
+     * 
+     * @param \Tfts\Match $match
+     * @param User $challenged
+     */
+    public function declineUserChallenge(Match $match, User $challenged) {
+        
+    }
+
+    /**
+     * The given players reports the result for the given match.
+     * 
+     * @param \Tfts\Match $match
+     * @param User $player
+     * @param type $score1 Score of the challenger.
+     * @param type $score2 Score of the challenged.
+     */
+    public function reportResultUserMatch(Match $match, User $player, $score1, $score2) {
+        
+    }
+
+    /**
+     * The open match is cancelled by the given user (can be done by both players).
+     * 
+     * @param \Tfts\Match $match
+     * @param User $player
+     */
+    public function cancelUserMatch(Match $match, User $player) {
+        
+    }
+
+    /**
+     * Processes the given match. If both parties have reported the result, the 
+     * match is closed and points will be awarded, otherwise this method does 
+     * nothing.
+     * 
+     * @param \Tfts\Match $match
+     */
+    private function processMatch(Match $match) {
+        
+    }
+
+    public function processTrackmaniaData() {
+        $data = [$_REQUEST, 'Data processed!'];
         return new JsonResponse($data);
     }
 
-    public function addPoints(User $user, $lan_id, $points){
+    public function addPoints(User $user, $lan_id, $points) {
         $ranking = $this->getUserRanking($user, $this->em->find('Tfts\Entity\Lan', 1));
         $ranking->addPoints($points);
     }
 
-    public function addPointsQB(User $user, $lan_id, $points){
+    public function addPointsQB(User $user, $lan_id, $points) {
         // For most older user Accounts that where migrated from the old system, we still have the user
         // id saved as a Attribute
         $ui = $user->getUserInfoObject();
         $oldUserId = $ui->getAttribute('old_user_id');
-        if(!empty($oldUserId) && is_numeric($oldUserId)){
+        if (!empty($oldUserId) && is_numeric($oldUserId)) {
 
             $qb = $this->db->createQueryBuilder();
             $query = $qb
-                ->update('t_lan_tfts_ranking')
-                ->set('points', 'points + ?')
-                ->where('user_id = ?')
-                ->setParameter(0, (integer)$points)
-                ->setParameter(1, (integer)$oldUserId);
+                    ->update('t_lan_tfts_ranking')
+                    ->set('points', 'points + ?')
+                    ->where('user_id = ?')
+                    ->setParameter(0, (integer) $points)
+                    ->setParameter(1, (integer) $oldUserId);
             $query->execute();
 
             $query = $this->qb
-                ->select('ltsr.points')
-                ->from('t_lan_tfts_ranking', 'ltsr')
-                ->where('ltsr.user_id = ?')
-                ->andWhere('ltsr.lan_id = ?')
-                ->setParameter(0, $user->getUserName())
-                ->setParameter(1, $lan_id);
+                    ->select('ltsr.points')
+                    ->from('t_lan_tfts_ranking', 'ltsr')
+                    ->where('ltsr.user_id = ?')
+                    ->andWhere('ltsr.lan_id = ?')
+                    ->setParameter(0, $user->getUserName())
+                    ->setParameter(1, $lan_id);
 
             $result = $query->execute()->FetchAll();
 
             return $result;
-
         }
         return false;
     }
