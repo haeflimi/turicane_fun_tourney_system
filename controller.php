eb458e93af759b062d557b8198e06c8cd6df705a
Freezer <lza@SCHW4097.schiller.ch>
2019-10-07 13:43:31 +0200

    Updated package version.

--- controller.php
+++ controller.php
@@ -15,7 +15,7 @@ class Controller extends Package implements ProviderAggregateInterface {
 
   protected $pkgHandle = 'turicane_fun_tourney_system';
   protected $appVersionRequired = '8.4';
-  protected $pkgVersion = '0.96';
+  protected $pkgVersion = '0.101';
   protected $em;
   protected $pkgAutoloaderRegistries = array(
       'src/Tfts' => '\Tfts'
