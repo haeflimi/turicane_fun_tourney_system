92318a23ee97b7b3fbaf47a26624c2d7f6afe227
Michael Häfliger <michael.haefliger@mesch.ch>
2019-10-11 03:29:01 +0200

    implement games as c5 pages with attributes and a first skeleton for pool block ui elements, remove team manager

--- controller.php
+++ controller.php
@@ -15,7 +15,7 @@ class Controller extends Package implements ProviderAggregateInterface {
 
   protected $pkgHandle = 'turicane_fun_tourney_system';
   protected $appVersionRequired = '8.4';
-  protected $pkgVersion = '0.120.25';
+  protected $pkgVersion = '0.120.26';
   protected $em;
   protected $pkgAutoloaderRegistries = array(
       'src/Tfts' => '\Tfts'
