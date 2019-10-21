b004253f66fb9b4652eaca165c67d12f943153fa
haeflimi <haefliger.michael@bluewin.ch>
2019-10-22 01:42:24 +0200

    change paypal implementation go make use of new

--- controller.php
+++ controller.php
@@ -20,7 +20,7 @@ class Controller extends Package
 
     protected $pkgHandle = 'turicane_fun_tourney_system';
     protected $appVersionRequired = '8.4';
-    protected $pkgVersion = '0.120.42';
+    protected $pkgVersion = '0.120.43';
     protected $em;
     protected $pkgAutoloaderRegistries = array(
         'src/Tfts' => '\Tfts',
